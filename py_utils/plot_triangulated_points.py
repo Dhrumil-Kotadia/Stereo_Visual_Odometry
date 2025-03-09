620d66db44393553d28f1af4d110a4d4d2bd19c1
Dhrumil-Kotadia <kotadiadhrumil@gmail.com>
2025-03-09 03:08:47 -0400

    Minor Updates

--- py_utils/plot_triangulated_points.py
+++ py_utils/plot_triangulated_points.py
@@ -6,24 +6,9 @@ f_x, f_y, f_z = feature_data[:, 0], feature_data[:,1], feature_data[:,2]
 vehicle_data = np.loadtxt("outputs/translation.txt", delimiter=",")
 v_x, v_y, v_z = vehicle_data[:,0], vehicle_data[:,1], vehicle_data[:,2]
 
-# # 3D plot of the triangulated points and camera poses
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(v_x, v_y, -v_z, c='b', label='Camera Poses')
-# ax.scatter(f_x, f_y, f_z, c='r', s=0.5, label='Triangulated Points')
-# ax.set_xlabel('X')
-# ax.set_ylabel('Y')
-# ax.set_zlabel('Z')
-# ax.set_xlim([-10, 60])
-# ax.set_ylim([-10, 60])
-# ax.set_zlim(-0.5, 100)
-# plt.title("Triangulated 3D Points")
-# plt.legend()
-# plt.show()
-
 # Code to have plt ion and update after each iteration of a loop. Read 1 from translation and 50 from triangulated points
-# plt.ion()
-# plt.show()
+plt.ion()
+plt.show()
 for i in range(26):
     # if i%2 == 0:
     #     continue
@@ -37,6 +22,6 @@ for i in range(26):
     plt.ylim([-0.5, 100])
     # plt.grid()
     plt.pause(0.1)
-    plt.savefig("/media/storage/lost+found/projects/cpp/stereo_visual_odometry/update_plot_" + str(i) + ".png")
-# plt.ioff()
-# plt.show()
+    # plt.savefig("update_plot_" + str(i) + ".png")
+plt.ioff()
+plt.show()
